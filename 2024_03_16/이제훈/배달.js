0a9bc40d07b7558236732f9f411cb0c06c5c7319
이제훈 <2eejehoon@gmail.com>
2024-03-15 20:39:59 +0900

    2024_03_16 배달 수정

--- 2024_03_16/이제훈/배달.js
+++ 2024_03_16/이제훈/배달.js
@@ -1,21 +1,21 @@
 function solution(N, road, K) {
-  const g = Array.from({ length: N + 1 }, () => Array(N + 1).fill(0));
-  for (let [a, b, c] of road) {
-    g[a][b] = g[a][b] === 0 ? c : Math.min(g[a][b], c);
-    g[b][a] = g[a][b] === 0 ? c : Math.min(g[a][b], c);
+  const g = Array.from({ length: N + 1 }, () => Array(N + 1).fill(Infinity));
+  for (const [a, b, c] of road) {
+    g[a][b] = g[a][b] === Infinity ? c : Math.min(g[a][b], c);
+    g[b][a] = g[b][a] === Infinity ? c : Math.min(g[b][a], c);
   }
-  const set = new Set();
-  const check = Array(N + 1).fill(false);
-  const dfs = (v, time) => {
-    set.add(v);
-    for (let i = 0; i < g[v].length; i++) {
-      if (!check[i] && time + g[v][i] <= K && g[v][i] > 0) {
-        check[i] = true;
-        dfs(i, time + g[v][i]);
-        check[i] = false;
+  for (let i = 1; i <= N; i++) {
+    for (let j = 1; j <= N; j++) {
+      for (let k = 1; k <= N; k++) {
+        g[j][k] = Math.min(g[j][k], g[j][i] + g[i][k]);
       }
     }
-  };
-  dfs(1, 0);
-  return set.size;
+  }
+  let answer = 1;
+  for (let i = 2; i <= N; i++) {
+    if (g[1][i] <= K) {
+      answer++;
+    }
+  }
+  return answer;
 }
